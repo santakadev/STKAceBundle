ecf46d82f7410f683ddf69f03ffc3a0a7d206d2e
Daniel Santamaria <santaka87@gmail.com>
2016-03-20 12:57:06 +0100

    fix dependency

--- DependencyInjection/STKAceExtension.php
+++ DependencyInjection/STKAceExtension.php
@@ -2,10 +2,10 @@
 
 namespace STK\AceBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\Config\FileLocator;
 
 class STKAceExtension extends Extension
 {
