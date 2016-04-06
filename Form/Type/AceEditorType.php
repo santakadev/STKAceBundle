737396d90087dce5181978671e0bd2e8472e3146
Daniel Santamaria <santaka87@gmail.com>
2016-04-06 20:23:34 +0200

    update textarea value on ace editor change event

--- Form/Type/AceEditorType.php
+++ Form/Type/AceEditorType.php
@@ -4,11 +4,21 @@ namespace STK\AceBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AceEditorType extends AbstractType
 {
     public function getParent()
     {
         return TextareaType::class;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'attr' => [
+                'class' => 'ace-editor-type'
+            ]
+        ]);
     }
 }
