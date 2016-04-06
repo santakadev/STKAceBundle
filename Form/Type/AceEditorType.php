eed87f28b8cc7c09adf5e1a1d01c9ccf005e934c
Daniel Santamaria <santaka87@gmail.com>
2016-04-06 21:07:37 +0200

    add mode option

--- Form/Type/AceEditorType.php
+++ Form/Type/AceEditorType.php
@@ -4,6 +4,8 @@ namespace STK\AceBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AceEditorType extends AbstractType
@@ -15,10 +17,18 @@ class AceEditorType extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver)
     {
+        $resolver->setDefined(array('mode'));
+
         $resolver->setDefaults([
+            'mode' => 'ace/mode/javascript',
             'attr' => [
                 'class' => 'ace-editor-type'
             ]
         ]);
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['mode'] = $options['mode'];
     }
 }
